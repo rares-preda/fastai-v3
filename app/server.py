07a251d274857b0f669e2533a9561e9942ca1998
Rares Mihai Preda <rares.preda.m@gmail.com>
2020-05-17 13:35:44 +0300

    deploy pkl file and classes after new fork

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://cdn-17.anonfile.com/xcU3jc0bob/dc8450db-1589674725/3plants.pkl'
+export_file_name = '3plants.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['eunonymus', 'petunia', 'thuja']
 path = Path(__file__).parent
 
 app = Starlette()
